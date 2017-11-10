f51725b56389adbf3a2062a6bcd4d4722ff264fd
Semen Shestakov <shestakovsemen@gmail.com>
2017-11-11 00:34:09 +0200

    Add WatchBird interface

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
+class WatchBird {
+  constructor() {
+    Object.assign(this, {
+      messages: [],
+    })
+  }
+  addMessage(msg, meta = {}) {
+    this.messages.push({msg, ...meta})
+  }
+  getRisks() {
+    return Math.random()
+  }
+}
+
+
 $(() => {
   const socket = io()
+  const wb = new WatchBird()
+
   $('form').submit(() => {
     const $m = $('#m')
 
@@ -10,6 +27,9 @@ $(() => {
   })
   socket.on('chat message', msg => {
     $('#messages').append($('<li>').text(msg))
+    wb.addMessage(msg)
+    $('#risk').html(JSON.stringify(wb.getRisks(), 2, 2))
+
     window.scrollTo(0, document.body.scrollHeight)
   })
 })
